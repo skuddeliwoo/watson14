a9aafe1593f2987b0a24826b79d2867c20624f8e
Andi <skuddeliwoo@gmail.com>
2020-06-11 09:38:18 +0200

    add grn recombination

--- src/@Population/recombine.m
+++ src/@Population/recombine.m
@@ -10,13 +10,15 @@ cumFitness = cumsum(fitness);
 % Create randomised list of fitness-selected parent indices:
 
 markers = rand(1,pop.nPop);
-[~,parents] = histc(markers,[0 cumFitness]);
+[~,~,parents] = histcounts(markers, [0 cumFitness]);
 parents = parents(randperm(pop.nPop));
 % parents = find
 
 % 1st half of parents are Mummies; 2nd half are Daddies:
-maternalGenotype = pop.genotype(:,parents(1:pop.nPop/2));
-paternalGenotype = pop.genotype(:,parents(pop.nPop/2+1:pop.nPop));
+maternal.genotype = pop.genotype(:,parents(1:pop.nPop/2));
+maternal.grn = pop.grn(:,:,parents(1:pop.nPop/2));
+paternal.genotype = pop.genotype(:,parents(pop.nPop/2+1:pop.nPop));
+paternal.grn = pop.grn(:,:,parents(1:pop.nPop/2));
 
 % Create crossover masks ...
 xover = false(pop.nTrait,pop.nPop/2);
@@ -29,12 +31,18 @@ myPX = 0.4;
 xover(:,rand(1,pop.nPop/2)>myPX) = false;
 
 % Perform crossover:
-girls = maternalGenotype;
-girls(xover) = paternalGenotype(xover);
-boys = paternalGenotype;
-boys(xover) = maternalGenotype(xover);
+girls.genotype = maternal.genotype;
+girls.grn = maternal.grn;
+girls.genotype(xover) = paternal.genotype(xover);
+girls.grn(xover) = paternal.grn(xover);
 
-pop.genotype(:,:) = [girls boys];
+boys.genotype = paternal.genotype;
+boys.grn = paternal.grn;
+boys.genotype(xover) = maternal.genotype(xover);
+boys.grn(xover) = maternal.grn(xover);
+
+pop.genotype(:,:) = [girls.genotype boys.genotype];
+pop.grn(:,:,:) = cat(3, girls.grn, boys.grn);
 
 end
 
